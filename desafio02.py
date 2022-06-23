6e10dea4f08913e42faf69055402bd721a7cecdd
gabrieldahora <santosdahoragabriel@gmail.com>
2022-06-23 19:12:53 -0300

    Create desafio02.py

---
+++ desafio02.py
@@ -0,0 +1,4 @@
+dia = input('Qual dia você nasceu? ')
+mes = input('Qual mês você nasceu? ')
+ano = input('Qual ano você nasceu? ')
+print ('Você nasceu no dia', dia, 'do mes', mes, 'do ano de', ano )
